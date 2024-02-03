a32924f1e2f050ef08a7c663fd1277ea6290e1c8
Jaber <jaberriyan357@gmail.com>
2024-02-03 10:44:29 +0600

    datetime attribute auto_now_add add

--- task/models.py
+++ task/models.py
@@ -7,7 +7,7 @@ class TaskModel(models.Model):
     task_title = models.CharField(max_length=200)
     task_description = models.TextField(max_length=1000)
     is_completed = models.BooleanField(default=False)
-    task_assign_date = models.DateTimeField(default=datetime.datetime.today())
+    task_assign_date = models.DateTimeField(auto_now_add=True)
     category = models.ManyToManyField(CategoryModel)
     
     def __str__(self):
